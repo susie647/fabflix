0ce6599518496c7ae10a52888a14c8baaf147e7a
susie647 <susiexiaoqi@gmail.com>
2020-05-02 22:00:30 -0700

    added dashboard page with table info

---
+++ src/main/java/DashboardServlet.java
@@ -0,0 +1,127 @@
+package main.java;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+@WebServlet(name = "DashboardServlet", urlPatterns = "/cs122b/dashboard")
+public class DashboardServlet extends HttpServlet {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+    //private DatabaseMetaData databaseMetaData;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(true);
+        response.setContentType("application/json"); // Response mime type
+
+        PrintWriter out = response.getWriter();
+
+        try {
+            Connection dbCon = dataSource.getConnection();
+            //databaseMetaData = dbCon.getMetaData();
+            //ResultSet rs = databaseMetaData.getTables(null,null,null,new String[]{"TABLE"});
+
+            //get all tables
+
+            Statement statement = dbCon.createStatement();
+            String query = "show tables";
+
+            ResultSet rs = statement.executeQuery(query);
+
+
+
+
+
+            JsonArray jsonArray = new JsonArray();
+            while(rs.next()){
+                //get metadata for each table
+                //String table = rs.getString("TABLE_NAME");
+
+                //ResultSet rsTable = databaseMetaData.getColumns(null,null, table, null);
+                String table = rs.getString("Tables_in_moviedb");
+                Statement statementTable = dbCon.createStatement();
+                String queryTable = String.format("describe %s", table);
+                //String queryTable = "describe creditcards";
+                ResultSet rsTable = statementTable.executeQuery(queryTable);
+
+
+                while(rsTable.next()){
+                    JsonObject jsonObject = new JsonObject();
+
+                    //String attribute = rsTable.getString("COLUMN_NAME");
+                    //String type = rsTable.getString("DATA_TYPE");
+                    String attribute = rsTable.getString("FIELD");
+                    String type = rsTable.getString("TYPE");
+
+                    jsonObject.addProperty("attribute", attribute);
+                    jsonObject.addProperty("type", type);
+                    jsonObject.addProperty("table",table);
+                    jsonArray.add(jsonObject);
+                }
+
+
+
+
+
+                rsTable.close();
+                statementTable.close();
+            }
+            // write JSON string to output
+            out.write(jsonArray.toString());
+
+
+
+
+
+
+/*
+            JsonArray jsonArray = new JsonArray();
+            while(rs.next()){
+                String table = rs.getString("TABLE_NAME");
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("table",table);
+                jsonArray.add(jsonObject);
+            }
+            out.write(jsonArray.toString());
+*/
+
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            rs.close();
+            statement.close();
+            dbCon.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        }
+
+        out.close();
+        //response.getWriter().write("success");
+    }
+}
---
+++ web/dashboard.js
@@ -0,0 +1,70 @@
+// let saleId = 13561;
+
+function handleDashboardData(resultData) {
+    console.log("handleGenresData: populating genres table from resultData");
+
+    // Genres body table
+    let DashboardTableBody = $("#dashboard_table_body");
+
+    /*
+    for (let i = 0; i < resultData.length; i++) {
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML += "<th>" + resultData[i]["table"]+ "</th>";
+        rowHTML += "<th>" + resultData[i]["attribute"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["type"] + "</th>";
+        rowHTML += "</tr>";
+        DashboardTableBody.append(rowHTML);
+    }
+
+     */
+
+    let dashboard_dict = {};
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < resultData.length; i++) {
+
+        let table = resultData[i]["table"];
+
+        if (dashboard_dict.hasOwnProperty(table) === false) {
+            dashboard_dict[table] = {"attribute": [resultData[i]["attribute"]], "type": [resultData[i]["type"]]};
+        }
+        else{
+            dashboard_dict[table]["attribute"].push(resultData[i]["attribute"]);
+            dashboard_dict[table]["type"].push(resultData[i]["type"]);
+        }
+    }
+
+    for (let table in dashboard_dict) {
+        for(let j=0; j<dashboard_dict[table]["attribute"].length; j++){
+            let rowHTML = "";
+            rowHTML += "<tr>";
+            //first occurence of table, show table
+            if(j===0){
+                rowHTML += "<th>" + table+ "</th>";
+            }
+            //print empty for table
+            else{
+                rowHTML += "<th>" + " "+ "</th>";
+            }
+
+            rowHTML += "<td>" + dashboard_dict[table]["attribute"][j] + "</td>";
+            rowHTML += "<td>" + dashboard_dict[table]["type"][j] + "</td>";
+            rowHTML += "</tr>";
+            DashboardTableBody.append(rowHTML);
+        }
+
+    }
+
+
+
+
+}
+
+
+
+$.ajax({
+    dataType: "json", // Setting return data type
+    method: "get", // Setting request method
+    url: "cs122b/dashboard",
+    success: (resultData) => handleDashboardData(resultData)
+});
