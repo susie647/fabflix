92369fb384d408eda1e72129f63d97e863033df6
Kanglan Tang <karenkanglantang@gmail.com>
2020-06-05 18:07:43 -0700

    check multiple log files

--- java_test/src/com/company/log_processing.java
+++ java_test/src/com/company/log_processing.java
@@ -10,24 +10,25 @@ public class log_processing
 //        System.out.println("The Average TS is: " + args);
 //        System.out.println("The Average TJ is: " + args[0]);
 
-
-        String filename = args[0]; //
-//                "/usr/local/Cellar/tomcat@8/8.5.53/libexec/webapps/cs122b-spring20-team125/log1.txt";
-        File file = new File(filename);
-
-        Scanner sc = new Scanner(file);
-
         ArrayList<Integer> TSs = new ArrayList<Integer>();
         ArrayList<Integer> TJs = new ArrayList<Integer>();
 
-        String st;
-        while (sc.hasNextLine())
-        {
-            st = sc.nextLine();
-            String[] arrOfStr = st.split(" | ");
-            TSs.add(Integer.parseInt(arrOfStr[0]));
-//            arrOfStr[1] = " | "
-            TJs.add(Integer.parseInt(arrOfStr[2]));
+        for(int i = 0; i < args.length; i++){
+            String filename = args[i]; //
+    //                "/usr/local/Cellar/tomcat@8/8.5.53/libexec/webapps/cs122b-spring20-team125/log1.txt";
+            File file = new File(filename);
+
+            Scanner sc = new Scanner(file);
+
+            String st;
+            while (sc.hasNextLine())
+            {
+                st = sc.nextLine();
+                String[] arrOfStr = st.split(" | ");
+                TSs.add(Integer.parseInt(arrOfStr[0]));
+    //            arrOfStr[1] = " | "
+                TJs.add(Integer.parseInt(arrOfStr[2]));
+            }
         }
 
         long totalTS = 0;
